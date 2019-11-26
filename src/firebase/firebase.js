5c053c53bb29224e5f5d27d56d2687284ee274a5
unknown <betadayz>
2019-11-26 12:03:17 +0100

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,24 +1,226 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyARYvRewcPFmIWhLuerifDzHsy57SY_bOQ",
-    authDomain: "expensify-c1e4f.firebaseapp.com",
-    databaseURL: "https://expensify-c1e4f.firebaseio.com",
-    projectId: "expensify-c1e4f",
-    storageBucket: "expensify-c1e4f.appspot.com",
-    messagingSenderId: "562959907713",
-    appId: "1:562959907713:web:d43d5374534956033f8680",
-    measurementId: "G-TF5CYSJJ85"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  };
 
  firebase.initializeApp(config);
 
- firebase.database().ref().set({
-     name: 'Sunday Joseph',
-     age: 26,
-     isSingle: false,
-     location: {
-         city: 'Lagos',
-         country: 'Nigeria'
-     }
- });
+ const database = firebase.database();
+
+ export { firebase, database as default };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  database.ref('expenses').on('child_removed', (snapshot) => {
+//      console.log(snapshot.key, snapshot.val());
+//  });
+
+//  database.ref('expenses').on('child_changed', (snapshot) => {
+//      console.log(snapshot.key, snapshot.val());
+//  });
+
+//  database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+//  database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//          snapshot.forEach((childSnapshot) => {
+//              expenses.push({
+//                  id: childSnapshot.key,
+//                  ...childSnapshot.val()
+//              });
+//          });
+    
+//          console.log(expenses)
+//  });
+
+//  database.ref('expenses').once('value').then((snapshot) => {
+//      const expenses = [];
+//      snapshot.forEach((childSnapshot) => {
+//          expenses.push({
+//              id: childSnapshot.key,
+//              ...childSnapshot.val()
+//          });
+//      });
+
+//      console.log(expenses)
+//  });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 100,
+//     createdAt: 7971599
+// });
+
+
+
+//  database.ref('notes').push({
+//      title: 'Course Topics',
+//      body: 'React Native, Angular, Python'
+//  });
+
+//  const firebaseNotes = {
+//      notes: {
+//          fdbht325svs: {
+//             title: 'First note!',
+//             body: 'This is my note'
+//          }, 
+//          dfbdfghgmj3ry: {
+//             title: 'Another note',
+//             body: 'This is my note'
+//          },
+//          75855978cdsfsd: {
+//             title: 'Special note',
+//             body: 'Breaking Limit'
+//          }
+//      }
+//  }
+
+//  const notes = [{
+//      id: '12',
+//      title: 'First note!',
+//      body: 'This is my note'
+//  }, {
+//      id: 'dfdf3',
+//      title: 'Another note',
+//      body: 'This is my note'
+//  }, {
+//      id: 'dfghgfj8',
+//      title: 'Special note',
+//      body: 'Breaking Limit'
+//  }];
+
+//  database.ref('notes').set(notes);
+
+
+
+//  database.ref('note').push({
+//      title: 'Course Topics',
+//      body: 'React Native, Angular, Python, Ruby'
+//  });
+
+//  const firebaseNotes = {
+//      notes: {
+//          fg52nfdn: {
+//              title: 'First note!',
+//              body: 'This is my note'
+//          },
+//          fbdfsbdfaf209138r: {
+//              title: 'Another note',
+//              body: 'This is my note'
+//          }
+//      }
+//  };
+
+//  const notes = [{
+//      id: '12',
+//      title: 'First note!',
+//      body: 'This is my note'
+//  }, {
+//      id: '723as',
+//      title: 'Another note',
+//      body: 'This is my note'
+//  }];
+
+ // database.ref('note').set(firebaseNotes)
+
+//  database.ref().on('value', (snapshot) => {
+//      const val = snapshot.val();
+//      console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+//  })
+
+//  const onValueChange =  database.ref().on('value', (snapshot) => {
+//      console.log(snapshot.val());
+//  }, (e) => {
+//      console.log('Error with data fetching', e);
+//  });
+
+//  setTimeout(() => {
+//      database.ref('age').set(20);
+//  }, 3500);
+
+//  setTimeout(() => {
+//     database.ref('age').set(onValueChange);
+// }, 7500);
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500);
+
+// database.ref('location/country').once('value').then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+// }).catch((e) => {
+//     console.log('Error fetching data', e);
+// });
+
+// database.ref().set({
+//     name: 'sunday Joseph',
+//     age: 29,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     isSingle: false,
+//     location: {
+//         city: 'Lagos',
+//         country: 'Nigeria'
+//     }
+// }).then(() => {
+//     console.log('Data is saved!');
+// }).catch((e)=>  {
+//     console.log('This failed.', e)
+// })
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Los Angeles'
+
+// })
+
+
+
+// database.ref().update({
+//     job: 'Manager',
+//     location: {
+//         city: 'Boston',
+//         country: 'United States of America',
+//         countryFrom: 'Nigeria' 
+//     }
+// })
+
+//database.ref('isSingle').set(null);
+
+// database.ref().remove().then(() => {
+//     console.log('Data was removed');
+// }).catch((e) => {
+//     console.log('Did not remove data', e);
+// });
